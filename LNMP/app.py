213154c8e90e688a52c03e1a27e3320d1df98083
M.Daenjel <daenjelkirigo@gmail.com>
2019-03-26 23:20:22 +0300

    removing credentials

--- LNMP/app.py
+++ LNMP/app.py
@@ -27,8 +27,8 @@ def mpesa():
     PartyA = parse_arg_from_requests('phoneNumber')
     print(amount,PartyA)
     
-    consumer_key = "FPjPl8OknAclG3TYOvAIanJroWGuMNfg"
-    consumer_secret = "Smqt7ZQ61Xlgav8P"
+    consumer_key = "YOUR CONSUMER KEY"
+    consumer_secret = "YOUR CONSUMER SECRET"
     api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 
     r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret),verify=False)
@@ -43,19 +43,19 @@ def mpesa():
     BusinessShortCode = '174379'
     #Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919' 
 
-    AccountReference = 'DAENJEL'
-    TransactionDesc = 'daenjel'
+    AccountReference = 'YOUR ACCT REF'
+    TransactionDesc = 'YOUR TRANS DESC'
     #Timestamp = date('YmdGis')
     #Password = base64_encode(BusinessShortCode.Passkey.Timestamp)
 
     # callback url
-    CallBackURL = 'http://26ebc457.ngrok.io/hooks/mpesa'
+    CallBackURL = 'YOUR CALLBALL URL'
 
     api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
     headers = { "Authorization": "Bearer %s" %access_token }
     request = {
       "BusinessShortCode": BusinessShortCode,
-      "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTkwMzIzMTUzNDM4",
+      "Password": "YOUR PASS KEY",
       "Timestamp": "20190323153438",
       "TransactionType": "CustomerPayBillOnline",
       "Amount": amount,
